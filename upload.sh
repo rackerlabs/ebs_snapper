38acc65527047e48c9476d02fa6ce4f59dbffae4
Martin Smith <martin@mbs3.org>
2016-06-21 14:43:31 +0100

    Make it easier to upload
    - Convert upload.sh scripts to use faws-cli friendly environment variables
    - Add lambda-uploader as a project requirement, since upload.sh uses it
    - Check tons of commands in the bash script, to be sure they exited successfully

--- upload.sh
+++ upload.sh
@@ -19,30 +19,66 @@
 # under the License.
 #
 
-DDI="$1"
-AWS_ACCOUNT_ID="$2"
-if [[ -z  $DDI  ]] || [[ -z $AWS_ACCOUNT_ID ]]; then
-  echo "Usage: ./upload.sh <DDI> <AWS_ACCOUNT_ID>";
-  exit
+if [[ -z $FAWS_RACKSPACE_ACCOUNT ]]; then
+  export FAWS_RACKSPACE_ACCOUNT=$1
+fi
+if [[ -z $FAWS_AWS_ACCOUNT ]]; then
+  export FAWS_AWS_ACCOUNT=$2
 fi
 
-faws login || exit 1
-echo "Ensuring you are authenticated to FAWS and setting environment variables"
-eval "$(faws -r $DDI env -a $AWS_ACCOUNT_ID)"
+if [[ -z $FAWS_RACKSPACE_ACCOUNT  ]] || [[ -z $FAWS_AWS_ACCOUNT ]]; then
+  echo "Usage: ./upload.sh <FAWS_RACKSPACE_ACCOUNT> <FAWS_AWS_ACCOUNT>";
+  exit 1
+fi
+
+FAWS_CLI_BIN=$(which faws)
+if [[ $? -ne 0 ]]; then
+  echo "Could not find the faws CLI. Please ensure it's in your path as 'faws'"
+  exit 2
+fi
+
+FAWS_CLI_BIN=$(which lambda-uploader)
+if [[ $? -ne 0 ]]; then
+  echo "Could not find the lambda-uploader CLI. Please ensure it's in your path as 'lambda-uploader'"
+  exit 2
+fi
+
+echo "Ensuring you are authenticated to FAWS"
+faws login
+if [[ $? -ne 0 ]]; then
+  echo "faws-cli login command failed. Try 'faws login' yourself."
+  exit 3
+fi
+
+echo "Setting environment variables"
+FAWS_ENV_OUTPUT=$(faws env)
+if [[ $? -ne 0 ]]; then
+  echo "faws-cli env command failed. Try 'faws env' yourself."
+  exit 4
+fi
+eval "$FAWS_ENV_OUTPUT"
 
 echo "Creating bucket if it does not exist"
-UNIQUE_ID="ebs-snapper-$AWS_ACCOUNT_ID"
+UNIQUE_ID="ebs-snapper-$FAWS_AWS_ACCOUNT"
 S3_BUCKET=$UNIQUE_ID
 aws s3 mb s3://$S3_BUCKET/
 
 echo "Building lambda zip file"
 rm -rf *.zip
 lambda-uploader --no-upload -r requirements.txt -x ebs_snapper/lambdas.py .
+if [[ $? -ne 0 ]]; then
+  echo "lambda-uploader command failed."
+  exit 5
+fi
 
 echo "Uploading to $S3_BUCKET"
 FILES="cloudformation.json lambda_function.zip"
 for f in $FILES; do
   aws s3 cp $f s3://$S3_BUCKET/
+  if [[ $? -ne 0 ]]; then
+    echo "aws s3 command failed on $f."
+    exit 6
+  fi
 done
 
 CF_S3_URL="https://s3.amazonaws.com/$S3_BUCKET/cloudformation.json"
@@ -57,6 +93,10 @@ else
   echo "Stack does not exist in CloudFormation, creating it fresh."
   PARAMS="ParameterKey=LambdaS3Bucket,ParameterValue=$S3_BUCKET,UsePreviousValue=False"
   aws cloudformation create-stack --stack-name $UNIQUE_ID --template-url $CF_S3_URL --parameters $PARAMS --capabilities CAPABILITY_IAM
+  if [[ $? -ne 0 ]]; then
+    echo "aws cloudformation command failed on $f."
+    exit 7
+  fi
 
   echo "Exiting, since the stack may take a while to create."
   echo "Re-run this command later to publish a new version."
@@ -73,10 +113,20 @@ for FUNC in $LAMBDA_FUNCTIONS; do
     --function-name $FUNC \
     --s3-key lambda_function.zip
 
+  if [[ $? -ne 0 ]]; then
+    echo "aws lambda update-function-code command failed on $f."
+    exit 8
+  fi
+
   echo "Publishing new code version for: $FUNC"
   aws lambda publish-version --function-name $FUNC \
     --code-sha-256 $SHA256_SUM \
     --description $VERSION
+
+  if [[ $? -ne 0 ]]; then
+    echo "aws lambda publish-version command failed on $f."
+    exit 9
+  fi
 done
 
 echo "Now, go configure some backups using ebs-snapper's configure subcommand!"
